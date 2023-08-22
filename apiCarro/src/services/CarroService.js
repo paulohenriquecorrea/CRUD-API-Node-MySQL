62f1a61cf4c01e4b91ce9c78b171f11a2bebbd54
unknown <ph.sistemas.ti@gmail.com>
2023-08-21 21:22:24 -0400

    Add consulta carros

--- apiCarro/src/services/CarroService.js
+++ apiCarro/src/services/CarroService.js
@@ -1,1 +1,15 @@
-module.exports = {};
+const db = require('../db');
+
+module.exports = {
+  buscarTodos: () => {
+    return new Promise((resolve, reject) => {
+      db.query('SELECT * FROM carros', (error, results) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(results);
+      });
+    });
+  },
+};
